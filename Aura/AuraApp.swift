3fa1a866033cc72a4c703dbf6dfd2d17356ed5fb
Jency <arockia.jency@pranathiss.com>
2026-02-10 14:14:20 +0530

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by Jency on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
